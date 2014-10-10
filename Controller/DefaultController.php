b4d8d39c69de004c8920c87ffa72b514ab1cdb21
Hamdi LAADHARI <killerwolf06@gmail.com>
2014-10-10 08:35:31 +0200

    WIP bundle

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -8,6 +8,8 @@ class DefaultController extends Controller
 {
     public function indexAction($name)
     {
+    	$api = $this->get('cyclocity.data');
+    	var_dump($api->getContracts());
         return $this->render('KillerwolfJCDecauxCyclocityBundle:Default:index.html.twig', array('name' => $name));
     }
 }
